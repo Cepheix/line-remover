1a8dbeefcb43c19b83084220884a5833755edb70
Piotr Ptak <piotr.a.ptak@capgemini.com>
2021-03-02 16:25:59 +0100

    add implementation of the remover

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -23,4 +26,57 @@ func main() {
 	fmt.Printf("Reading lines from %v \n", options.SourceFile)
 	fmt.Printf("Removing lines from %v \n", options.DeleteTemplate)
 	fmt.Printf("Saving output to %v \n", options.OutputFile)
+
+	sourceLines, _ := readLines(options.SourceFile)
+	deleteLines, _ := readLines(options.DeleteTemplate)
+
+	result := make(map[string]bool)
+
+	for _, sourceLine := range sourceLines {
+		result[sourceLine] = true
+
+		for _, deleteLine := range deleteLines {
+			if strings.Contains(sourceLine, deleteLine) {
+				result[sourceLine] = false
+			}
+		}
+	}
+
+	output, err := os.Create(options.OutputFile)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for resultLine, shouldPrint := range result {
+		if shouldPrint {
+			fmt.Fprintln(output, resultLine)
+		}
+	}
+
+	err = output.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("file written successfully")
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text != "" {
+			lines = append(lines, text)
+		}
+	}
+	return lines, scanner.Err()
 }
